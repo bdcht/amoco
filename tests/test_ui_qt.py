9f635106c64fc22b334daeb2d8e752a33aadf588
Axel Tillequin <bdcht3@gmail.com>
2016-03-12 23:33:50 +0100

    remove test_ui_qt from py.test scope

--- tests/test_ui_qt.py
+++ tests/test_ui_qt.py
@@ -1,16 +1,12 @@
-import amoco
-from amoco.ui.graphics.qt_.engine import *
-amoco.ui.graphics.configure(graphics="qt")
-
-p = amoco.system.loader.load_program('samples/x86/flow.elf')
-z = amoco.lbackward(p)
-z.getcfg()
-f = z.G.C[2].sV[0].data.misc['func']
-
-w = MainWindow(z)
-w.createFuncGraph(f)
-#gs = GraphScene(f.view.layout)
-#gv = GraphView(gs)
-#gs.Draw()
-#gv.show()
-w.show()
+#import amoco
+#from amoco.ui.graphics.qt_.engine import *
+#amoco.ui.graphics.configure(graphics="qt")
+#
+#p = amoco.system.loader.load_program('samples/x86/flow.elf')
+#z = amoco.lbackward(p)
+#z.getcfg()
+#f = z.G.C[2].sV[0].data.misc['func']
+#
+#w = MainWindow(z)
+#w.createFuncGraph(f)
+#w.show()

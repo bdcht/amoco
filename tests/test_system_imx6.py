05675b82c8f6f494d85fe098943467f7ac664ecf
Axel Tillequin <bdcht3@gmail.com>
2019-01-19 19:05:18 +0100

    fix typo

--- tests/test_system_imx6.py
+++ tests/test_system_imx6.py
@@ -5,7 +5,7 @@ def test_header():
     h = HAB_Header(b"\xd1\x00\x20\x40")
     assert h.tag == HAB_TAG_IVT
     assert h.tag == 0xd1
-    assert h.len == 32
+    assert h.length == 32
     assert h.version == 0x40
 
 
